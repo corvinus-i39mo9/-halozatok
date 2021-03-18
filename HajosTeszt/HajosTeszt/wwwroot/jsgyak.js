0ad51478ffef394790ef084dca1c1b69ac1ce4f3
Márta Emese Orbán <marta.orban@stud.uni-corvinus.hu>
2021-03-18 17:24:05 +0100

    jsgyak.js updated

--- HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -5,20 +5,28 @@ window.onload = function () {
         let ujsor = document.createElement("div");
         x.appendChild(ujsor);
         ujsor.classList.add("sor");
-        ujsor.setAttribute("id", `${sor}`);
+        ujsor.setAttribute("id", `${sor+1}`);
         for (var oszlop = 0; oszlop <= sor; oszlop++) {
             let ujelem = document.createElement("div");
             ujsor.appendChild(ujelem);
-            if (sor < 2) {
-                ujelem.innerHTML = `1`
-                break;
+            try {
+                if (sor < 3) {
+                    ujelem.innerHTML = `1`;
+                    document.getElementById("32").innerHTML = '2';
+                }
+                else {
+                    let seged1 = (sor && oszlop);
+                    let seged2 = (sor && oszlop + 1);
+                    let seged3 = document.getElementById(seged1).innerText;
+                    let seged4 = document.getElementById(seged2).innerText;
+                    let faktorialis = fakt(seged3, seged4);
+                    ujelem.innerText = faktorialis;
+                }
+            } catch (e) {
+                ujelem.innerHTML = `1`;
             }
-            else {
-                let seged1 = (sor - 1 && oszlop - 1);
-                let seged2 = (sor - 1 && oszlop + 1);
-                fakt(document.getElementById(seged1).innerHTML, document.getElementById(seged2).innerHTML);
-            }
-            ujelem.setAttribute("id", `${sor}${oszlop}`);
+            
+            ujelem.setAttribute("id", `${sor+1}${oszlop+1}`);
             ujelem.classList.add("elem");
             ujelem.style.backgroundColor = `rgb(255,${255 / 10 * sor},${255 / 10 * -oszlop})`;
         }
